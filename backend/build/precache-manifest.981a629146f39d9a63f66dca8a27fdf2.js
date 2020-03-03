24e30398dd9289393420faf24de64f694af5e3c8
alexkindushenko <alikson22@gmail.com>
2020-03-03 18:33:56 +0200

    rebuild client

---
+++ backend/build/precache-manifest.981a629146f39d9a63f66dca8a27fdf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14cd88d93e7787b5bd8301e36497114a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d53a66c6fd26a039a8f",
+    "url": "/static/css/main.de81e568.chunk.css"
+  },
+  {
+    "revision": "3d0adaac7bd1130ce2ec",
+    "url": "/static/js/2.30c9da36.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/static/js/2.30c9da36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d53a66c6fd26a039a8f",
+    "url": "/static/js/main.5eb1b7f3.chunk.js"
+  },
+  {
+    "revision": "83832cfadea88956af11",
+    "url": "/static/js/runtime-main.3bfa6a7a.js"
+  }
+]);
